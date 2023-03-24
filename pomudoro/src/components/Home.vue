f948a005cd8d8b474637ea9e634682058ef6fa3b
Lorenzo-Ibay <lorenzo.ibay@gmail.com>
2023-03-24 16:23:32 +0800

    Dogmu and Pogmu added

--- pomudoro/src/components/Home.vue
+++ pomudoro/src/components/Home.vue
@@ -88,7 +88,10 @@
           d="M160.17,0A172,172,0,0,0,0,161.51"
         />
       </svg>
-      <h2>{{timeDisplay}}</h2>
+      <div class="centerDisplay">
+        <p v-if="resting">Rest</p>
+        <h2>{{timeDisplay}}</h2>
+      </div>
     </div>
     <button @click="startClick">{{ buttonText }}</button>
   </div>
@@ -97,13 +100,18 @@
 
 <script>
 import ProgressBar from 'progressbar.js';
+import Dogmu from "../assets/Dogmu.mp3";
+import amPomu from "../assets/amPomu.mp3";
+
 export default {
     name: "homeComponent",
     data: function(){
-      const workSplit = 25;
+      const workSplit = .05 * 60;
       // const breakSplit = 5;
       return{
-        currentTimeSec:workSplit * 60,
+        workSplit:workSplit,
+        restSplit: .05 * 60,
+        currentTimeSec:workSplit,
         currentSegment: 1,
         buttonText: "Start!",
         topRight:null,
@@ -112,9 +120,12 @@ export default {
         topLeft:null,
         pathOptions: {
         easing: 'linear',
-        duration: workSplit * 60 * 1000
+        duration: (workSplit * 1000) + 500
         },
         interval: null,
+        alert: new Audio(Dogmu),
+        restAlert: new Audio(amPomu),
+        resting: false,
       };
     },
     mounted: function() {
@@ -134,23 +145,70 @@ export default {
           this.buttonText = "Pause";
           this.animateBar();
         } else if (this.buttonText === "Pause"){
+          this.pauseBar();
           this.buttonText = "Resume";
         }
       },
       animateBar(){
-        this.interval = setInterval(() => {
-          this.currentTimeSec -= 1;
-        }, 1000);
+        this.reduceTime();
+        let segment = null;
         switch(this.currentSegment){
-          case 1: this.topRight.animate(0 , this.onFinish); break;
-          case 2: this.bottomRight.animate(0 , this.onFinish); break;
-          case 3: this.bottomLeft.animate(0 , this.onFinish); break;
-          case 4: this.topLeft.animate(0 , this.onFinish); break;
+          case 1: segment = this.topRight; break;
+          case 2: segment = this.bottomRight; break;
+          case 3: segment =  this.bottomLeft; break;
+          case 4: segment =  this.topLeft; break;
+        }
+        segment.animate( 0, {duration: (this.currentTimeSec + 1) * 1000}, this.onFinish);
+      },
+      pauseBar(){
+        clearInterval(this.interval);
+        switch(this.currentSegment){
+          case 1:this.topRight.stop(); break;
+          case 2:this.bottomRight.stop();break;
+          case 3:this.bottomLeft.stop();break;
+          case 4:this.topLeft.stop();break;
         }
       },
       onFinish(){
+       if(this.currentTimeSec <= 0){
         clearInterval(this.interval);
+        if(this.currentSegment <4 ){
+          this.currentSegment += 1;
+        }else {
+          this.topRight.set(1);
+          this.topLeft.set(1);
+          this.bottomRight.set(1);
+          this.bottomLeft.set(1);
+
+          this.currentSegment = 1;
+        }
+        this.alert.play();
+        this.resting= true;
+        this.buttonText = "Rest";
+        setTimeout(() => {
+          this.buttonText = "Start!";
+        this.currentTimeSec = this.restSplit;
+        this.startRest();
+        }, 3500);
+       }
       },
+      reduceTime(){
+        this.interval = setInterval(() => {
+          if(this.currentTimeSec > 0){
+            this.currentTimeSec -= 1;
+          }
+        }, 1000);
+      },
+      startRest(){
+        this.reduceTime();
+        setTimeout(() => {
+          clearInterval(this.interval);
+          this.restAlert.play();
+          this.currentTimeSec = this.workSplit;
+          this.buttonText = "Start!"
+          this.resting = false;
+        }, this.restSplit * 1000);
+      }
     },
     computed: {
       timeDisplay(){
@@ -184,12 +242,25 @@ h1{
     margin-left: 10px;
 }
 h2{
+  font-size: 64px;
+  color: #f85959;
+}
+p{
+  font-size: 48px;
+  line-height: 48px;
+  text-align: center;
+  color: #ff8080;
+
+}
+.centerDisplay {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%,-50%);
-  font-size: 64px;
-  color: #f85959;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 .timer{
     position: relative;

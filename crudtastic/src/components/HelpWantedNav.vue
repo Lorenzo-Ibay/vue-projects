5b6dac1537ed5b9ef52fbf0ed54b42f13c6e1d08
lorenzo-ibay <lorenzo.ibay@gmail.com>
2023-05-08 16:22:20 +0800

    Cleanup and project initiation

---
+++ crudtastic/src/components/HelpWantedNav.vue
@@ -0,0 +1,21 @@
+<template>
+   <header>
+    <nav class="container">
+        <div class="branding">
+            <img src="../assets/" alt="">
+            <h1>Help Wanted</h1>
+        </div>
+        <ul class="nav-routes">
+            <RouterLink to="/">Home</RouterLink>
+        </ul>
+    </nav>
+   </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+
+</style>

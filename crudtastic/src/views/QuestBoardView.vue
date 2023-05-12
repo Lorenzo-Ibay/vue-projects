514b50db850ab22aa8735bc88d9ecdd8adf138df
lorenzo-ibay <lorenzo.ibay@gmail.com>
2023-05-12 16:05:45 +0800

    Learning Methods ref vs reactive

---
+++ crudtastic/src/views/QuestBoardView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import JobPoster from '../components/JobPoster.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Post Quest</h1>
+    <JobPoster />
+  </main>
+</template>
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
